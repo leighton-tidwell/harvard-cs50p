560d0fa4d50d4e052e48b8906a42d39b3458f820
Leighton Tidwell <leighton@tidwells.world>
2025-01-27 21:36:30 -0600

    playing with split

--- week-0/hello.py
+++ week-0/hello.py
@@ -2,7 +2,10 @@
 # created as part of Harvards CS50P
 
 # Ask the user for their name
-name = input("What's your name? ")
+name = input("What's your name? ").strip().title()
+
+# Split users name into first name and last name
+first, last = name.split(" ")
 
 # Say hello to user
-print(f"hello, {name}")
+print(f"hello, {first}")
