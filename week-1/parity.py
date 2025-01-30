f6ccadd02ce244f17a07f9dba087d65b67278b16
Leighton Tidwell <leighton@tidwells.world>
2025-01-29 21:46:14 -0600

    using function with return val for bool

--- week-1/parity.py
+++ week-1/parity.py
@@ -1,6 +1,15 @@
-x = int(input("What's x? "))
+def main():
+  x = int(input("What's x? "))
+  if is_even(x):
+    print("Even")
+  else:
+    print("Odd")
 
-if x % 2 == 0:
-  print("Even")
-else:
-  print("Odd")
+
+def is_even(n):
+  if n % 2 == 0:
+    return True
+  else:
+    return False
+
+main()
