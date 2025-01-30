dd8bd85a24a954019327493bbac024ad1f60da7f
Leighton Tidwell <leighton@tidwells.world>
2025-01-29 21:53:40 -0600

    using match

---
+++ week-1/house.py
@@ -0,0 +1,11 @@
+name = input("What's your name? ")
+
+match name:
+  case "Harry":
+    print("Gryffindor")
+  case "Hermione":
+    print("Gryffindor")
+  case "Ron":
+    print("Gryffindor")
+  case "Draco":
+    print("Slytherin")
