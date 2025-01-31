ab7c1ecdea6bfc5e02180404aaa96a458ad54e9f
Leighton Tidwell <leighton@tidwells.world>
2025-01-30 20:10:25 -0600

    all loops

--- week-2/cat.py
+++ week-2/cat.py
@@ -1,1 +1,15 @@
-print("meow\n" * 3, end="")
+def main():
+  number = get_number()
+  meow(number)
+
+def get_number():
+  while True:
+    n = int(input("What's n?"))
+    if n > 0:
+      return n
+
+def meow(n):
+  for _ in range(n):
+    print("meow")
+
+main()
