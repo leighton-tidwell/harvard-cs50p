79a99e4bb10bdb6aa8cedeb6a9eafd157e622fb0
Leighton Tidwell <leighton@tidwells.world>
2025-01-30 20:24:41 -0600

    dicts

--- week-2/hogwarts.py
+++ week-2/hogwarts.py
@@ -1,5 +1,9 @@
-students = ["Hermione","Harry","Ron"]
+students = {
+  "Hermione": "Gryffindor", 
+  "Harry": "Gryffindor", 
+  "Ron": "Gryffindor",
+  "Draco": "Slytherin"
+}
 
-for i in range(len(students)):
-  print(i + 1, students[i])
-
+for student in students:
+  print(student, students[student], sep=", ")
